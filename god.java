04aa0d4568dda421f91bf358475ba9c866591bdc
Aakash-S-P <aakashpalanisamy11@gmail.com>
2025-03-06 12:08:46 +0530

    ADD

---
+++ god.java
@@ -0,0 +1,14 @@
+package Arrays;
+import java.util.Scanner;
+
+public class god {
+public static void main(String[] args) {
+    Scanner arg =new Scanner(System.in);
+    int a=arg.nextInt();
+    int b=arg.nextInt();
+    System.out.println(a+b);
+
+}
+
+    
+}
