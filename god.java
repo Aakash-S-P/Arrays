72b7ef9a15aaf5e8300289a2eae32d4db9958abf
Aakash S P <aakashpalanisamy11@gmail.com>
2025-03-06 12:10:24 +0530

    Update god.java

--- god.java
+++ god.java
@@ -1,12 +1,12 @@
-package Arrays;
+
 import java.util.Scanner;
 
 public class god {
 public static void main(String[] args) {
     Scanner arg =new Scanner(System.in);
-    int a=arg.nextInt();
+    int c=arg.nextInt();
     int b=arg.nextInt();
-    System.out.println(a+b);
+    System.out.println(c+b);
 
 }
 
